ac4dceeb91e8536bb338f6c11ae1ce19fc5d9988
Chris Warren <16132615+c-warren@users.noreply.github.com>
2025-09-30 11:17:37 -0700

    feat: Add a 404 endpoint (#16)

--- main.go
+++ main.go
@@ -16,6 +16,10 @@ func main() {
 		fmt.Fprintf(w, "Hello World")
 	})
 
+	http.HandleFunc("/404", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Not an actual not_found")
+	})
+
 	fmt.Println("Server starting on :8080")
 	http.ListenAndServe(":8080", nil)
 }
